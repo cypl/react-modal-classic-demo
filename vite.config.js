12d2b3708cbc02519c7eecb935b3e69ea4637e1a
cypl <cyrille.plenchette@me.com>
2023-08-24 09:57:51 +0200

    remove alias and dedupe

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-function getCurrentDir(metaUrl) {
-  return path.dirname(new URL(metaUrl).pathname)
-}
-
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'styled-components': path.resolve(getCurrentDir(import.meta.url), 'node_modules', 'styled-components'),
-    },
-    dedupe: ['styled-components']
-  },
 })
