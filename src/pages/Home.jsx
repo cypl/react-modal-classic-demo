d94d51e980d2b2edafb53e1acf3a808a91793bec
cypl <cyrille.plenchette@me.com>
2023-08-24 10:03:02 +0200

    version condition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -171,7 +171,7 @@ function Home(){
         <>
         <Header>
             <div className="content_large">
-                <p>React Modal Classic <span>{version}</span></p>
+                <p>React Modal Classic {version.length > 0 && <span>{version}</span>}</p>
                 <p className="repositories">
                     <a href={npmUrl} target="_blank" rel="noreferrer" className="npmLink">
                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M288 288h-32v-64h32v64zm288-128v192H288v32H160v-32H0V160h576zm-416 32H32v128h64v-96h32v96h32V192zm160 0H192v160h64v-32h64V192zm224 0H352v128h64v-96h32v96h32v-96h32v96h32V192z"/></svg>
